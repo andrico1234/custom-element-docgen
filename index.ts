67b54f5d9bc44dbd6951b6230e4d10632c3c6ebe
Andrico <karoulla.andrico@gmail.com>
2024-05-14 08:41:35 +0100

    minor changes

--- index.ts
+++ index.ts
@@ -4,14 +4,13 @@ import { generateResults } from './customElementManifest/generateResults';
 import { formatResults } from './customElementManifest/formatResults';
 import fs from 'node:fs';
 import { fileURLToPath } from 'node:url';
-import { resolve, dirname, join } from "node:path";
+import { dirname, join } from "node:path";
 import { withPageData } from './routing/withPageData';
 import { withUsageData } from './usage/withUsageData';
 
 const PAGE_PATTERN = 'components/[component]';
 
 // TODO: what's the best way to handle this?
-
 const cwd = dirname(fileURLToPath(import.meta.url))
 const PATH_TO_DATA_JSON = join(cwd, './data.json')
 
@@ -29,18 +28,14 @@ const createPlugin = ({
   return {
     name: 'custom-elements-docgen',
     hooks: {
-      'astro:config:setup': async ({ injectRoute, updateConfig, config }) => {
+      'astro:config:setup': async ({ injectRoute, updateConfig, injectScript, config, logger }) => {
         const results = await generateResults(pathToComponents);
 
         if (!results) {
           // TODO: beter error handling
-          console.error('No components could be found, skipping custom-element-docgen integration.');
+          logger.error('No components could be found, skipping custom-element-docgen integration.');
           return;
         }
-
-
-
-        // resolve(fileURLToPath(root), id) : id)
 
         const mergedComponents = {
           ...defaultComponents,
@@ -68,6 +63,19 @@ const createPlugin = ({
           pattern: PAGE_PATTERN,
           entrypoint: mergedComponents.Page
         })
+
+        for await (const component of resultsWithUsageData) {
+          const { registerPath } = component.props.usage;
+
+          try {
+
+            // TODO: I'm, not able to build scripts dynamically
+            const data = await fs.promises.readFile(registerPath, 'utf8')
+            injectScript('before-hydration', data);
+          } catch (err) {
+            logger.error(`Error reading file: ${registerPath}`);
+          }
+        }
       }
     }
   }
